0696b9f5d4da8375186c440f817a17dfb891803e
toumi245 <77539701+toumi245@users.noreply.github.com>
2023-05-25 16:21:38 +0100

    Update server.js

--- BackEND/server.js
+++ BackEND/server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
     app.use(express.static('FronEnd/build'));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'FronEnd', 'build','index.html')));
+    app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'FronEnd', 'build','index.html')));
   }
 app.use(notFound)
 app.use(errorHandler)
